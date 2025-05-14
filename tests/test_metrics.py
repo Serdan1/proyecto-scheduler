19376aa9bac40e999315f9db3a294936d6b1c635
Daniel Serrano Martin <danielserranoreal@gmail.com>
2025-05-14 07:38:18 +0000

    test_metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -5,7 +5,7 @@ from src.metrics import Metricas
 def test_metricas():
     procesos = [Proceso("p1", 3, 1), Proceso("p2", 2, 2)]
     procesos[0].tiempo_inicio = 0
-    procesos[0].     procesos[0].tiempo_fin = 3
+    procesos[0].tiempo_fin = 3
     procesos[1].tiempo_inicio = 3
     procesos[1].tiempo_fin = 5
     gantt = [("p1", 0, 3), ("p2", 3, 5)]
